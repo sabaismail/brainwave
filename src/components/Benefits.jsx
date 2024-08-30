a1711d09a41d4ee8e88bb60bcc42e7614d62b1d6
sabaismail <sabaismail011@gmail.com>
2024-08-30 17:39:51 +0500

    updated

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -14,7 +14,7 @@ const Benefits = () => {
                 className=" md:max-w-md lg:max-w-2xl"
                 title="Chat Smarter, Not Harder with Brainwave"
             />
-            <div className='flex flex-wrap gap-10 mb-10'>
+            <div className='flex flex-wrap gap-10 mb-10 items-center justify-center'>
                 {benefits.map((item) => (
                     <div className='block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]' style={{
                         backgroundImage: `url(${item.backgroundUrl})`
